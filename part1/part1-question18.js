affc32b4f480c62bf7bf37def6ddd7da01570af2
hwaleh <hwaleh@ucsd.edu>
2021-01-29 22:56:17 -0800

    q18

--- part1/part1-question18.js
+++ part1/part1-question18.js
@@ -1,15 +1,14 @@
 
-/*for (const key in object) {
+let statistics= {
+    redCars: 21,
+    blueCars: 45,
+    greencars: 12,
+    racecars: 5,
+    blackCars: 40,
+    rareCars: 2
+};
+for (const key in statistics) {
     if(1==1){}          //condition
-    console.log(`${key}: ${object[key]}`);  //print
+    console.log(`${key}: ${statistics[key]}`);  //print
     
-}*/
-if(2==true){
-    console.log('hello');
-}
-else if(2){
-    console.log('how are u');
-}
-else{
-    console.log('bye');
 }
